572d51b52facec9238fbb7bc7ca3855eb5814862
Jorge Baumann <rambo@rambitoJS.local>
2020-05-16 13:06:00 +0200

    Init Vuepress :bowtie:

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -31,14 +31,13 @@ module.exports = {
     nextLinks: true,
     prevLinks: true,
     smoothScroll: true,
+    lastUpdated: 'Last Updated',
     // Assumes GitHub. Can also be a full GitLab url.
     repo: 'baumannzone/awesome-browser-apis',
     // Customising the header label
     // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
     repoLabel: 'Contribute!',
-
     // Optional options for generating "Edit this page" link
-
     // if your docs are in a different repo from your main project:
     // docsRepo: 'vuejs/vuepress',
     // if your docs are not at the root of the repo:
