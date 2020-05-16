85682e677bc79aaea6d315ba785081ed43af8b4e
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-05-16 18:05:40 +0200

    Create Example3.vue

---
+++ src/.vuepress/components/API/Audio/Example3.vue
@@ -0,0 +1,28 @@
+<template>
+  <BaseExample>
+    <b-button variant="primary" size="sm" @click="demo">White Noise</b-button>
+  </BaseExample>
+</template>
+
+<script>
+  import BaseExample from "../../BaseExample"
+
+  export default {
+    name: 'AudioExample3',
+    components: {
+      BaseExample
+    },
+    methods: {
+      demo() {
+        const ac = new AudioContext()
+        const oscillator = ac.createOscillator()
+
+        oscillator.connect(ac.destination)
+
+        const time = ac.currentTime
+        oscillator.start(time)
+        oscillator.stop(time + 1)
+      }
+    }
+  }
+</script>
