7d23438c88adaa79aeaba074adbceea116722df1
Jorge Baumann <>
2020-05-16 17:20:00 +0200

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -111,12 +111,11 @@ module.exports = {
     }],
     [
       'link', {
-      rel: 'stylesheet',
-      href: 'https://use.fontawesome.com/releases/v5.8.1/css/all.css',
-      integrity: 'sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf',
-      crossorigin: 'anonymous'
-    }
+        rel: 'stylesheet',
+        href: 'https://use.fontawesome.com/releases/v5.8.1/css/all.css',
+        integrity: 'sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf',
+        crossorigin: 'anonymous'
+      }
     ]
-
   ],
 }
