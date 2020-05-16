6ae9b132edd96ab79874861445a3dd04092b1777
Jorge Baumann <>
2020-05-16 17:06:44 +0200

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -14,38 +14,16 @@ module.exports = {
     nav: [
       {text: 'Home', link: '/'},
       {text: 'About', link: '/about/'},
-      // {
-      //   text: 'Languages',
-      //   ariaLabel: 'Language Menu',
-      //   items: [
-      //     {text: 'Chinese', link: '/language/chinese/'},
-      //     {text: 'Japanese', link: '/language/japanese/'}
-      //   ]
-      // }
     ],
-    // sidebar: [
-    //   ['/api/audio/', 'AUDIO']
-    //   ['/catering/appetizers/', 'APPETIZERS'],
-    // ],
     nextLinks: true,
     prevLinks: true,
     smoothScroll: true,
     lastUpdated: 'Last Updated',
-    // Assumes GitHub. Can also be a full GitLab url.
     repo: 'baumannzone/awesome-browser-apis',
-    // Customising the header label
-    // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
     repoLabel: 'Contribute!',
-    // Optional options for generating "Edit this page" link
-    // if your docs are in a different repo from your main project:
-    // docsRepo: 'vuejs/vuepress',
-    // if your docs are not at the root of the repo:
     docsDir: 'src',
-    // if your docs are in a specific branch (defaults to 'master'):
     docsBranch: 'master',
-    // defaults to false, set to true to enable
     editLinks: true,
-    // custom text for edit link. Defaults to "Edit this page"
     editLinkText: 'Help us improve this page!'
   },
   plugins: [
