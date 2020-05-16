ac10666ad8260c19087630a2e452d4fb88e5b78c
Jorge Baumann <>
2020-05-16 17:07:01 +0200

    Create Example1.vue

---
+++ src/.vuepress/components/API/Audio/Example1.vue
@@ -0,0 +1,31 @@
+<template>
+  <BaseExample>
+    <b-button variant="primary" size="sm" @click="demo">Sound</b-button>
+  </BaseExample>
+</template>
+
+<script>
+import BaseExample from "../../BaseExample"
+
+export default {
+  name: 'AudioExample1',
+  components: {
+    BaseExample
+  },
+  methods: {
+    demo () {
+      const ac = new AudioContext()
+      const oscillator = ac.createOscillator()
+      oscillator.connect(ac.destination)
+
+      const time = ac.currentTime
+      oscillator.start(time)
+      oscillator.stop(time + 1)
+
+      oscillator.onended = () => {
+        console.log('Stopped!')
+      }
+    }
+  }
+}
+</script>
