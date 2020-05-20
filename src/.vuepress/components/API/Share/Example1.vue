9aa6bd341ebd65c7c80bf5d22d34a264b5b629e1
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-05-20 21:16:42 +0200

    Create Example1.vue

---
+++ src/.vuepress/components/API/Share/Example1.vue
@@ -0,0 +1,47 @@
+<template>
+  <BaseExample>
+    <b-button variant="primary" size="sm" @click="demo">Share 🎖</b-button>
+    <hr>
+    <p v-if="text">{{ text }}</p>
+  </BaseExample>
+</template>
+
+<script>
+import BaseExample from "../../BaseExample"
+
+export default {
+  name: 'AudioExample1',
+  components: {
+    BaseExample
+  },
+  data() {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    demo () {
+      this.message = ''
+      if (navigator.share) {
+        const data = {
+          title: 'Awesome Browser API\'s by @Baumannzone',
+          text: '🔴 Join me and watch @baumannzone on #YoutubeLive. Awesome Browser API\'s #YoMeQuedoEnCasaJS',
+          url: 'https://www.youtube.com/watch?v=5O5ppW9DcNE'
+        }
+
+        navigator.share(data)
+          .then(() => {
+            this.message = 'Successful share'
+            console.log('Share 👌')
+          })
+          .catch((error) => {
+            this.message = error
+            console.log('Error sharing:', error)
+          })
+      } else {
+        console.log('Navigator.share not supported')
+      }
+    }
+  }
+}
+</script>
