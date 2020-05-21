47e05ff456ff3e3b9bedecf791611421555c2f21
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-05-21 10:01:00 +0200

    Speech WIP

---
+++ src/.vuepress/components/API/Speech/Example2.vue
@@ -0,0 +1,39 @@
+<template>
+  <BaseExample>
+    <b-form @submit="demo">
+
+      <b-form-group label="Text:" label-for="inputDemo1">
+        <b-form-input id="inputDemo1" v-model="form.input" placeholder="Type something.." autocomplete="off"></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary" size="sm">Submit</b-button>
+    </b-form>
+  </BaseExample>
+</template>
+
+<script>
+  import BaseExample from "../../BaseExample"
+
+  export default {
+    name: 'SpeechExample2',
+    components: {
+      BaseExample
+    },
+    data() {
+      return {
+        form: {
+          input: ''
+        }
+      }
+    },
+    methods: {
+      demo (ev) {
+        ev.preventDefault()
+        const text = this.form.input
+        const synth = window.speechSynthesis
+        const utterThis = new SpeechSynthesisUtterance(text)
+        synth.speak(utterThis)
+      }
+    }
+  }
+</script>
